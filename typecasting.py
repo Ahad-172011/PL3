c9109d177192699e9f24665f7b2a86f875c8552c
Ahad-172011 <ahadace1@gmail.com>
2025-06-12 19:12:16 +0530

    ggk

---
+++ typecasting.py
@@ -0,0 +1,14 @@
+name = "penguin"
+age = 15
+is_student = True
+weight = 38.5
+
+print("\n after Type Casting....")
+age = str(age)
+print("Data Type of age is", type(age))
+weight = int(weight)
+print("Data Type of Weight is", type(weight))
+
+num = input("enter any num")
+print("hello", num, "how are you?")
+print(type(num))
